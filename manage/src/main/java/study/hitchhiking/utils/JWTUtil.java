16f9c26cd01e2f7e1caf767f6b803d7b5632fe06
28977 <2897754450@qq.com>
2023-03-24 12:58:05 +0800

    manage模块增加JWT鉴权

---
+++ manage/src/main/java/study/hitchhiking/utils/JWTUtil.java
@@ -0,0 +1,26 @@
+package study.hitchhiking.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.sql.Date;
+import java.util.Map;
+
+public class JWTUtil {
+    private static final long EXPIRE_TIME = 1800000L;
+    private static final String SECRET = "ffaagoicgioIUIUOAOUI(!23454KLJTADW:??{}{|】]l'ad,'q[5fdf>?N<:{LWPW";
+    private static final Algorithm ALGORITHM = Algorithm.HMAC256(SECRET);
+    public static String signWithUID(String userID){
+        return JWT.create().withClaim("userID",userID)
+                .withExpiresAt(new Date(System.currentTimeMillis()+EXPIRE_TIME))
+                .sign(ALGORITHM);
+    }
+
+
+    public static String getUIDByToken(String token){
+        DecodedJWT jwt = JWT.decode(token);
+        return jwt.getClaim("userID").asString();
+    }
+}
---
+++ manage/src/test/java/study/hitchhiking/utils/JWTTest.java
@@ -0,0 +1,17 @@
+package study.hitchhiking.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JWTTest {
+    @Test
+    public void should_sign_success(){
+        String testTarget = "123";
+        String token = JWTUtil.signWithUID(testTarget);
+        Assertions.assertNotEquals(testTarget,token);
+        String unSignTarget = JWTUtil.getUIDByToken(token);
+        Assertions.assertEquals(testTarget,unSignTarget);
+    }
+}
