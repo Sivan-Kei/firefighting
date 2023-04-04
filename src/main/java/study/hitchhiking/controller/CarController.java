2e5e50105fd4e47d0965ceb9a7be9dc5b393fb15
28977 <2897754450@qq.com>
2023-04-04 13:14:23 +0800

    加入JWT鉴权

---
+++ src/main/java/study/hitchhiking/controller/CarController.java
@@ -0,0 +1,21 @@
+package study.hitchhiking.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 吴建豪
+ * @since 2023-03-24
+ */
+@RestController
+@RequestMapping("/car")
+public class CarController {
+
+}
+
